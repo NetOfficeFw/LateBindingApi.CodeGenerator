9754a645e5992902938d350561b7239ae132f51a
SebastianDotNet <SebastianDotNet@codeplex.com>
2011-04-12 17:51:41 +0000

    extend EnumerableApi
    Imported from https://latebindingapi.codeplex.com/SourceControl/changeset/71739

--- latebindingapi/LateBindingApi.CodeGenerator.CSharp/EnumerableApi.cs
+++ latebindingapi/LateBindingApi.CodeGenerator.CSharp/EnumerableApi.cs
@@ -90,6 +90,11 @@ namespace LateBindingApi.CodeGenerator.CSharp
                  return true;
         }
 
+        /// <summary>
+        ///  add enumerator code
+        /// </summary>
+        /// <param name="faceNode"></param>
+        /// <param name="content"></param>
         internal static void AddEnumerator(XElement faceNode, ref string content)
         {
             XElement returnType = GetEnumNode(faceNode).Element("Parameters").Element("ReturnValue");
@@ -130,43 +135,76 @@ namespace LateBindingApi.CodeGenerator.CSharp
             if ("true" == returnType.Attribute("IsArray").Value)
                 type += "[]";
 
-            // get type optionals
-            if ("true" == returnType.Attribute("IsEnum").Value)
-            {
-                if ("true" == returnType.Attribute("IsExternal").Value)
-                {
-                    type = "externalprojectnamespace.enums." + type;
-                }
-                else
-                {
-                    type = faceNode.Parent.Parent.Attribute("Namespace").Value + ".Enums." + type;
-                }
-            }
-            else if ("true" == returnType.Attribute("IsComProxy").Value)
-            {
-                if (("COMObject" == returnType.Attribute("Type").Value) || ("COMObject" == returnType.Attribute("Type").Value) )
-                {
- 
-                }
-                else if ("true" == returnType.Attribute("IsExternal").Value)
-                {
-                    type = "externalprojectnamespace." + type;
-                }
-                else
-                {
-                    type = faceNode.Parent.Parent.Attribute("Namespace").Value + type;
-                }
-            }
-           
+            // get type qualifiers
+            string qualifier = GetQualifier(faceNode, returnType);
+            type = qualifier + type;
+
             enumString = enumString.Replace("%Type%", type);
 
             content += enumString;
         }
 
+        /// <summary>
+        ///  remove enumerator %% marker
+        /// </summary>
+        /// <param name="content"></param>
         internal static void RemoveEnumeratorMarker(ref string content)
         {
             content = content.Replace("%enumerableSpace%", "");
             content = content.Replace("%enumerable%", "");
+        }
+
+        /// <summary>
+        /// get qualifier of type
+        /// </summary>
+        /// <param name="faceNode"></param>
+        /// <param name="returnType"></param>
+        /// <returns></returns>
+        internal static string GetQualifier(XElement faceNode, XElement returnType)
+        {
+            if(("COMObject" == returnType.Attribute("Type").Value) || ("COMObject" == returnType.Attribute("Type").Value))
+                return "";
+
+            if ("true" == returnType.Attribute("IsEnum").Value)
+            {
+                if ("true" == returnType.Attribute("IsExternal").Value)
+                {
+                    string refProjectKey = returnType.Element("ProjectKey").Value;
+                    if ("" != refProjectKey)
+                    { 
+                        XElement projectNode = (from a in returnType.Element("LateBindingApi.CodeGenerator.Document").Element("Solution").Element("Projects").Elements("Project")
+                                              where a.Attribute("Key").Value.Equals(refProjectKey)
+                                              select a).FirstOrDefault();
+                        return projectNode.Attribute("Namespace").Value + ".enums.";
+                    }
+
+
+                }
+                else
+                {
+                    return faceNode.Parent.Parent.Attribute("Namespace").Value + ".Enums." ;
+                }
+            }
+            else if ("true" == returnType.Attribute("IsComProxy").Value)
+            {
+                if ("true" == returnType.Attribute("IsExternal").Value)
+                {
+                    string refProjectKey = returnType.Element("ProjectKey").Value;
+                    if ("" != refProjectKey)
+                    {
+                        XElement projectNode = (from a in returnType.Element("LateBindingApi.CodeGenerator.Document").Element("Solution").Element("Projects").Elements("Project")
+                                                where a.Attribute("Key").Value.Equals(refProjectKey)
+                                                select a).FirstOrDefault();
+                        return projectNode.Attribute("Namespace").Value + ".";
+                    }
+                }
+                else
+                {
+                    return  faceNode.Parent.Parent.Attribute("Namespace").Value;
+                }
+            }
+
+            return "";
         }
     }
 }
