bedd9897cb5e20c84eb5761fa55978325b4c03bf
Jozef Izso <jozef.izso@gmail.com>
2017-01-12 12:11:50 +0100

    Update texts on NetOffice Cheats form

--- LateBindingGui/Forms/FormNetOffice.Designer.cs
+++ LateBindingGui/Forms/FormNetOffice.Designer.cs
@@ -53,18 +53,20 @@
             // okButton
             // 
             this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.okButton.Location = new System.Drawing.Point(419, 437);
+            this.okButton.Location = new System.Drawing.Point(838, 840);
+            this.okButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.okButton.Name = "okButton";
-            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.Size = new System.Drawing.Size(150, 44);
             this.okButton.TabIndex = 24;
             this.okButton.Text = "&OK";
             this.okButton.Click += new System.EventHandler(this.okButton_Click);
             // 
             // textBoxAssemblyVersion
             // 
-            this.textBoxAssemblyVersion.Location = new System.Drawing.Point(102, 105);
+            this.textBoxAssemblyVersion.Location = new System.Drawing.Point(204, 202);
+            this.textBoxAssemblyVersion.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.textBoxAssemblyVersion.Name = "textBoxAssemblyVersion";
-            this.textBoxAssemblyVersion.Size = new System.Drawing.Size(78, 20);
+            this.textBoxAssemblyVersion.Size = new System.Drawing.Size(152, 31);
             this.textBoxAssemblyVersion.TabIndex = 25;
             this.textBoxAssemblyVersion.Text = "1.5.1.2";
             // 
@@ -73,9 +75,10 @@
             this.checkBoxAssemblyVersion.AutoSize = true;
             this.checkBoxAssemblyVersion.Checked = true;
             this.checkBoxAssemblyVersion.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.checkBoxAssemblyVersion.Location = new System.Drawing.Point(31, 108);
+            this.checkBoxAssemblyVersion.Location = new System.Drawing.Point(62, 208);
+            this.checkBoxAssemblyVersion.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.checkBoxAssemblyVersion.Name = "checkBoxAssemblyVersion";
-            this.checkBoxAssemblyVersion.Size = new System.Drawing.Size(65, 17);
+            this.checkBoxAssemblyVersion.Size = new System.Drawing.Size(123, 29);
             this.checkBoxAssemblyVersion.TabIndex = 26;
             this.checkBoxAssemblyVersion.Text = "Enabled";
             this.checkBoxAssemblyVersion.UseVisualStyleBackColor = true;
@@ -85,9 +88,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(27, 86);
+            this.label1.Location = new System.Drawing.Point(54, 165);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(117, 16);
+            this.label1.Size = new System.Drawing.Size(219, 30);
             this.label1.TabIndex = 27;
             this.label1.Text = "Assembly Version";
             // 
@@ -95,9 +99,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(27, 138);
+            this.label2.Location = new System.Drawing.Point(54, 265);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(113, 16);
+            this.label2.Size = new System.Drawing.Size(208, 30);
             this.label2.TabIndex = 28;
             this.label2.Text = "Set Namespaces";
             // 
@@ -106,9 +111,10 @@
             this.checkBoxNamespaces.AutoSize = true;
             this.checkBoxNamespaces.Checked = true;
             this.checkBoxNamespaces.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.checkBoxNamespaces.Location = new System.Drawing.Point(30, 157);
+            this.checkBoxNamespaces.Location = new System.Drawing.Point(60, 302);
+            this.checkBoxNamespaces.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.checkBoxNamespaces.Name = "checkBoxNamespaces";
-            this.checkBoxNamespaces.Size = new System.Drawing.Size(65, 17);
+            this.checkBoxNamespaces.Size = new System.Drawing.Size(123, 29);
             this.checkBoxNamespaces.TabIndex = 29;
             this.checkBoxNamespaces.Text = "Enabled";
             this.checkBoxNamespaces.UseVisualStyleBackColor = true;
@@ -118,9 +124,10 @@
             this.checkBoxCallQuit.AutoSize = true;
             this.checkBoxCallQuit.Checked = true;
             this.checkBoxCallQuit.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.checkBoxCallQuit.Location = new System.Drawing.Point(31, 205);
+            this.checkBoxCallQuit.Location = new System.Drawing.Point(62, 394);
+            this.checkBoxCallQuit.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.checkBoxCallQuit.Name = "checkBoxCallQuit";
-            this.checkBoxCallQuit.Size = new System.Drawing.Size(65, 17);
+            this.checkBoxCallQuit.Size = new System.Drawing.Size(123, 29);
             this.checkBoxCallQuit.TabIndex = 31;
             this.checkBoxCallQuit.Text = "Enabled";
             this.checkBoxCallQuit.UseVisualStyleBackColor = true;
@@ -129,20 +136,22 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(28, 186);
+            this.label3.Location = new System.Drawing.Point(56, 358);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(216, 16);
+            this.label3.Size = new System.Drawing.Size(624, 30);
             this.label3.TabIndex = 30;
-            this.label3.Text = "Call quit in Dispose for applications";
+            this.label3.Text = "Call Quit() method in Dispose() for Application objects";
             // 
             // checkBoxAnalyzeReturn
             // 
             this.checkBoxAnalyzeReturn.AutoSize = true;
             this.checkBoxAnalyzeReturn.Checked = true;
             this.checkBoxAnalyzeReturn.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.checkBoxAnalyzeReturn.Location = new System.Drawing.Point(31, 256);
+            this.checkBoxAnalyzeReturn.Location = new System.Drawing.Point(62, 492);
+            this.checkBoxAnalyzeReturn.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.checkBoxAnalyzeReturn.Name = "checkBoxAnalyzeReturn";
-            this.checkBoxAnalyzeReturn.Size = new System.Drawing.Size(65, 17);
+            this.checkBoxAnalyzeReturn.Size = new System.Drawing.Size(123, 29);
             this.checkBoxAnalyzeReturn.TabIndex = 33;
             this.checkBoxAnalyzeReturn.Text = "Enabled";
             this.checkBoxAnalyzeReturn.UseVisualStyleBackColor = true;
@@ -151,20 +160,22 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(28, 237);
+            this.label4.Location = new System.Drawing.Point(56, 456);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(465, 16);
+            this.label4.Size = new System.Drawing.Size(898, 30);
             this.label4.TabIndex = 32;
-            this.label4.Text = "AnaylzeReturn=False [COMAddin.Object, _CustomTaskPane.ContentControl]";
+            this.label4.Text = "AnalyzeReturn=False [COMAddin.Object, _CustomTaskPane.ContentControl]";
             // 
             // checkBoxEarlyBind
             // 
             this.checkBoxEarlyBind.AutoSize = true;
             this.checkBoxEarlyBind.Checked = true;
             this.checkBoxEarlyBind.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.checkBoxEarlyBind.Location = new System.Drawing.Point(30, 305);
+            this.checkBoxEarlyBind.Location = new System.Drawing.Point(60, 587);
+            this.checkBoxEarlyBind.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.checkBoxEarlyBind.Name = "checkBoxEarlyBind";
-            this.checkBoxEarlyBind.Size = new System.Drawing.Size(65, 17);
+            this.checkBoxEarlyBind.Size = new System.Drawing.Size(123, 29);
             this.checkBoxEarlyBind.TabIndex = 35;
             this.checkBoxEarlyBind.Text = "Enabled";
             this.checkBoxEarlyBind.UseVisualStyleBackColor = true;
@@ -173,19 +184,21 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(27, 286);
+            this.label5.Location = new System.Drawing.Point(54, 550);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(568, 16);
+            this.label5.Size = new System.Drawing.Size(1087, 30);
             this.label5.TabIndex = 34;
             this.label5.Text = "EarlyBind=True [IRibbonUI, IRibbonControl, IRibbonExtensibility, ICustomTaskPaneC" +
-                "onsumer]";
+    "onsumer]";
             // 
             // cancelButton
             // 
             this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.cancelButton.Location = new System.Drawing.Point(500, 437);
+            this.cancelButton.Location = new System.Drawing.Point(1000, 840);
+            this.cancelButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.cancelButton.Name = "cancelButton";
-            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.Size = new System.Drawing.Size(150, 44);
             this.cancelButton.TabIndex = 36;
             this.cancelButton.Text = "&Cancel";
             this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
@@ -195,9 +208,10 @@
             this.checkBoxRangeType.AutoSize = true;
             this.checkBoxRangeType.Checked = true;
             this.checkBoxRangeType.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.checkBoxRangeType.Location = new System.Drawing.Point(31, 353);
+            this.checkBoxRangeType.Location = new System.Drawing.Point(62, 679);
+            this.checkBoxRangeType.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.checkBoxRangeType.Name = "checkBoxRangeType";
-            this.checkBoxRangeType.Size = new System.Drawing.Size(65, 17);
+            this.checkBoxRangeType.Size = new System.Drawing.Size(123, 29);
             this.checkBoxRangeType.TabIndex = 38;
             this.checkBoxRangeType.Text = "Enabled";
             this.checkBoxRangeType.UseVisualStyleBackColor = true;
@@ -206,9 +220,10 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(28, 334);
+            this.label6.Location = new System.Drawing.Point(56, 642);
+            this.label6.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(262, 16);
+            this.label6.Size = new System.Drawing.Size(495, 30);
             this.label6.TabIndex = 37;
             this.label6.Text = "Range.Default, Range.Item to Type Range";
             // 
@@ -217,9 +232,10 @@
             this.checkBoxSupportByVersion.AutoSize = true;
             this.checkBoxSupportByVersion.Checked = true;
             this.checkBoxSupportByVersion.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.checkBoxSupportByVersion.Location = new System.Drawing.Point(31, 402);
+            this.checkBoxSupportByVersion.Location = new System.Drawing.Point(62, 773);
+            this.checkBoxSupportByVersion.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.checkBoxSupportByVersion.Name = "checkBoxSupportByVersion";
-            this.checkBoxSupportByVersion.Size = new System.Drawing.Size(65, 17);
+            this.checkBoxSupportByVersion.Size = new System.Drawing.Size(123, 29);
             this.checkBoxSupportByVersion.TabIndex = 40;
             this.checkBoxSupportByVersion.Text = "Enabled";
             this.checkBoxSupportByVersion.UseVisualStyleBackColor = true;
@@ -228,20 +244,22 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(28, 383);
+            this.label7.Location = new System.Drawing.Point(56, 737);
+            this.label7.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(215, 16);
+            this.label7.Size = new System.Drawing.Size(520, 30);
             this.label7.TabIndex = 39;
-            this.label7.Text = "Analyze and set SupportByVersion";
+            this.label7.Text = "Analyze and set [SupportByVersion] attribute";
             // 
             // checkBoxSolutionName
             // 
             this.checkBoxSolutionName.AutoSize = true;
             this.checkBoxSolutionName.Checked = true;
             this.checkBoxSolutionName.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.checkBoxSolutionName.Location = new System.Drawing.Point(31, 54);
+            this.checkBoxSolutionName.Location = new System.Drawing.Point(62, 104);
+            this.checkBoxSolutionName.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.checkBoxSolutionName.Name = "checkBoxSolutionName";
-            this.checkBoxSolutionName.Size = new System.Drawing.Size(65, 17);
+            this.checkBoxSolutionName.Size = new System.Drawing.Size(123, 29);
             this.checkBoxSolutionName.TabIndex = 42;
             this.checkBoxSolutionName.Text = "Enabled";
             this.checkBoxSolutionName.UseVisualStyleBackColor = true;
@@ -249,9 +267,10 @@
             // 
             // textBoxSolutionName
             // 
-            this.textBoxSolutionName.Location = new System.Drawing.Point(102, 51);
+            this.textBoxSolutionName.Location = new System.Drawing.Point(204, 98);
+            this.textBoxSolutionName.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.textBoxSolutionName.Name = "textBoxSolutionName";
-            this.textBoxSolutionName.Size = new System.Drawing.Size(78, 20);
+            this.textBoxSolutionName.Size = new System.Drawing.Size(152, 31);
             this.textBoxSolutionName.TabIndex = 41;
             this.textBoxSolutionName.Text = "NetOffice";
             // 
@@ -259,9 +278,10 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(28, 32);
+            this.label8.Location = new System.Drawing.Point(56, 62);
+            this.label8.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(96, 16);
+            this.label8.Size = new System.Drawing.Size(181, 30);
             this.label8.TabIndex = 43;
             this.label8.Text = "Solution Name";
             // 
@@ -269,18 +289,19 @@
             // 
             this.label9.AutoSize = true;
             this.label9.ForeColor = System.Drawing.Color.Red;
-            this.label9.Location = new System.Drawing.Point(329, 318);
+            this.label9.Location = new System.Drawing.Point(658, 612);
+            this.label9.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(238, 13);
+            this.label9.Size = new System.Drawing.Size(469, 25);
             this.label9.TabIndex = 44;
             this.label9.Text = "IRTDServer, IRTDUpdate Outlook.PropertyPage";
             // 
             // FormNetOffice
             // 
             this.AcceptButton = this.okButton;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(604, 472);
+            this.ClientSize = new System.Drawing.Size(1208, 908);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.checkBoxSolutionName);
@@ -303,10 +324,11 @@
             this.Controls.Add(this.textBoxAssemblyVersion);
             this.Controls.Add(this.okButton);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "FormNetOffice";
-            this.Padding = new System.Windows.Forms.Padding(9);
+            this.Padding = new System.Windows.Forms.Padding(18, 17, 18, 17);
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
