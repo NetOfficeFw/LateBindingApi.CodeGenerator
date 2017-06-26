7492493ff4f5090e34366a9b3c39cd3b445ac4a3
Jozef Izso <jozef.izso@gmail.com>
2017-06-26 16:41:20 +0200

    Add app for generating NetOffice source code from command line.

---
+++ CodeGen/Options.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CommandLine;
+
+namespace LateBindingApi.CodeGenerator.CodeGen
+{
+    public class Options
+    {
+        [Option('o', "output", DefaultValue = "out", HelpText = "Output folder for generated code")]
+        public string OutputFolder { get; set; }
+
+        public static bool TryParse(string[] args, out Options options)
+        {
+            options = new Options();
+            if (args != null)
+            {
+                return Parser.Default.ParseArguments(args, options);
+            }
+
+            return true;
+        }
+    }
+}
---
+++ CodeGen/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NLog;
+
+namespace LateBindingApi.CodeGenerator.CodeGen
+{
+    class Program
+    {
+        private static readonly Logger Log = LogManager.GetLogger(nameof(Program));
+
+        public static void Main(string[] args)
+        {
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(UnhandledException);
+
+            Options options;
+            if (!Options.TryParse(args, out options))
+            {
+                Log.Error("Failed to parse command line arguments.");
+                Environment.Exit(CommandLine.Parser.DefaultExitCodeFail);
+            }
+
+            Log.Info($@"Generating code to '{options.OutputFolder}'");
+            Log.Info("Done.");
+            Console.ReadKey();
+        }
+
+        private static void UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            var ex = e.ExceptionObject as Exception;
+            Log.Error(ex, $"Unhandled exception in codegen.exe (IsTerminating={e.IsTerminating}).");
+        }
+    }
+}
