3b9851933e5504a3903560bf2304e16973a7137c
Jozef Izso <jozef.izso@gmail.com>
2018-12-16 00:28:51 +0100

    Generate NetOffice.sln file matching the format from the v1.7.4.3 release

--- CodeGenerator.CSharp/SolutionApi.cs
+++ CodeGenerator.CSharp/SolutionApi.cs
@@ -9,11 +9,12 @@ namespace LateBindingApi.CodeGenerator.CSharp
 {
     internal static class SolutionApi
     {
-        internal static readonly string _projectLine = "Project(\"{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}\") = \"%Name%Api\",\"%Name%\\%Name%Api.csproj\", \"{%Key%}\"\r\n%Depend%EndProject\r\n";
+        internal static readonly string _projectLine = "Project(\"{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}\") = \"%Name%Api\", \"%Name%\\%Name%Api.csproj\", \"{%Key%}\"\r\n%Depend%EndProject\r\n";
 
         internal static readonly string _buildConfig = "\t\t{%Key%}.Debug|Any CPU.ActiveCfg = Debug|Any CPU\r\n"
                                                      + "\t\t{%Key%}.Debug|Any CPU.Build.0 = Debug|Any CPU\r\n"
-                                                     + "\t\t{%Key%}.Release|Any CPU.ActiveCfg = Release|Any CPU\r\n";
+                                                     + "\t\t{%Key%}.Release|Any CPU.ActiveCfg = Release|Any CPU\r\n"
+                                                     + "\t\t{%Key%}.Release|Any CPU.Build.0 = Release|Any CPU\r\n";
 
         internal static string ReplaceSolutionAttributes(Settings settings, string solutionFile, XElement solution)
         {
@@ -33,7 +34,7 @@ namespace LateBindingApi.CodeGenerator.CSharp
 
             if (true == settings.AddTestApp)
             {
-                string testProjectLine = "Project(\"{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}\") = \"%Name%\",\"%Name%\\%Name%.csproj\", \"{%Key%}\"\r\n%Depend%EndProject\r\n";
+                string testProjectLine = "Project(\"{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}\") = \"%Name%\", \"%Name%\\%Name%.csproj\", \"{%Key%}\"\r\n%Depend%EndProject\r\n";
                 string newProjectLine = testProjectLine.Replace("%Name%", "ClientApplication");
                 newProjectLine = newProjectLine.Replace("%Key%", "DF73F99F-DFC0-42D1-9EDF-AD7D890C53D5");
 
@@ -56,38 +57,52 @@ namespace LateBindingApi.CodeGenerator.CSharp
                 if ("true" == project.Attribute("Ignore").Value)
                     continue;
 
-                string newProjectLine = _projectLine.Replace("%Name%", project.Attribute("Name").Value);
+                string projectName = project.Attribute("Name").Value;
+                string newProjectLine = _projectLine.Replace("%Name%", projectName);
                 newProjectLine = newProjectLine.Replace("%Key%", CSharpGenerator.ValidateGuid(project.Attribute("Key").Value));
                 string depends = "";
-                if(project.Element("RefProjects").Elements("RefProject").Count() > 0)
-                    depends += "\tProjectSection(ProjectDependencies) = postProject\r\n";
-
                 foreach (var item in project.Element("RefProjects").Elements("RefProject"))
                 {
                     string projKey = item.Attribute("Key").Value;
                     XElement projNode = (from a in solution.Element("Projects").Elements("Project")
                                         where a.Attribute("Key").Value.Equals(projKey)
                                         select a).FirstOrDefault();
+
+                    // skip referenced project which are ignored by generator
+                    if ("true" == projNode.Attribute("Ignore").Value)
+                        continue;
+
                     string line = "\t\t" + "{%Key%} = {%Key%}" + "\r\n";
-                    depends += line.Replace("%Key%", CSharpGenerator.ValidateGuid(projNode.Attribute("Key").Value));
+                    string refProjectGuid = CSharpGenerator.ValidateGuid(projNode.Attribute("Key").Value);
+                    depends += line.Replace("%Key%", refProjectGuid);
                 }
 
-                if (project.Element("RefProjects").Elements("RefProject").Count() > 0)
-                    depends += "\tEndProjectSection\r\n";
+                if (!String.IsNullOrEmpty(depends))
+                {
+                    depends = "\tProjectSection(ProjectDependencies) = postProject\r\n"
+                            + depends
+                            + "\tEndProjectSection\r\n";
+                }
 
                 newProjectLine = newProjectLine.Replace("%Depend%", depends);
                 projects += newProjectLine;
 
                 string newConfig = _buildConfig.Replace("%Key%", CSharpGenerator.ValidateGuid(project.Attribute("Key").Value));
                 configs += newConfig;
+
+                // HACK: special hack to order NetOffice project after VBIDEApi to match the official solution file
+                if (projectName == "VBIDE")
+                {
+                    string newProjectLine2 = _projectLine.Replace("Api", "").Replace("%Name%", "NetOffice").Replace("%Key%", "65442327-D01F-4ECB-8C39-6D5C7622A80F").Replace("%Depend%", "");
+                    projects += newProjectLine2;
+
+                    string newConfig2 = _buildConfig.Replace("%Key%", "65442327-D01F-4ECB-8C39-6D5C7622A80F");
+                    configs += newConfig2;
+                }
             }
 
-            string newProjectLine2 = _projectLine.Replace("Api", "").Replace("%Name%", "NetOffice").Replace("%Key%", "65442327-D01F-4ECB-8C39-6D5C7622A80F").Replace("%Depend%", "");
-            projects += newProjectLine2;
-
-            string newConfig2 = _buildConfig.Replace("%Key%", "65442327-D01F-4ECB-8C39-6D5C7622A80F");
-            configs += newConfig2;
-
+            projects = projects.TrimEnd();
+            configs = configs.TrimEnd();
             solutionFile = solutionFile.Replace("%Projects%", projects);
             solutionFile = solutionFile.Replace("%Config%", configs);
             return solutionFile;
