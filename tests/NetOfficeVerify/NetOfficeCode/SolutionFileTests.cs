82c686a727062a9c59ddc1c25558e33009e25d85
Jozef Izso <jozef.izso@gmail.com>
2018-12-23 17:34:42 +0100

    Add verification tests project

---
+++ tests/NetOfficeVerify/NetOfficeCode/SolutionFileTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using NUnit.Framework;
+
+namespace NetOfficeVerify.NetOfficeCode
+{
+    [TestFixture]
+    public class SolutionFileTests : GeneratorTestContext
+    {
+        public static readonly string NetOfficeSolutionName = "NetOffice.sln";
+
+        [Test]
+        public void SolutionFile_MatchesGoldFile()
+        {
+            // Arrange
+            var generatedSolutionFile = Path.Combine(this.GeneratedCodeDir, NetOfficeSolutionName);
+            var goldSolutionFile = Path.Combine(this.GoldCodeDir, NetOfficeSolutionName);
+
+            // Act
+            // nop
+
+            // Assert
+            FileAssert.AreEqual(goldSolutionFile, generatedSolutionFile);
+        }
+    }
+}
