c59af6dadfe0ab431211bf0ed673f50ac38099d4
Jozef Izso <jozef.izso@gmail.com>
2019-01-05 19:16:57 +0100

    Refactor Settings classes to use auto properties

--- CodeGenerator.CSharp/Settings.cs
+++ CodeGenerator.CSharp/Settings.cs
@@ -1,184 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace LateBindingApi.CodeGenerator.CSharp
 {
     public class Settings
     {
-        #region Fields
-
-        string _folder;
-        bool   _convertOptionalsToObject;
-        bool   _convertParamNamesToCamelCase;
-        bool   _removeRefAttribute;
-        bool   _createDocu;
-        string _framework;
-        bool   _addTestApp;
-        bool   _openFolder;
-        bool  _useSigning;
-        bool _addDocumentationLinks = true;
-        string _linkFilePath;
-        string _signPath;
-
-        #endregion
-
         public Settings()
         {
+            this.AddDocumentationLinks = true;
             this.SupportByVersionSpacing = 1;
         }
 
-        #region Properties
+        // ReSharper disable once InconsistentNaming
+        public bool VBOptimization { get; set; }
 
-        public bool VBOptimization{get;set;}
+        public string SignPath { get; set; }
 
-        public string SignPath
-        {
-            get
-            {
-                return _signPath;
-            }
-            set
-            {
-                _signPath = value;
-            }
-        }
+        public string LinkFilePath { get; set; }
 
-        public string LinkFilePath
-        {
-            get
-            {
-                return _linkFilePath;
-            }
-            set
-            {
-                _linkFilePath = value;
-            }
+        public bool AddDocumentationLinks { get; set; }
 
-        }
-        public bool AddDocumentationLinks
-        {
-            get
-            {
-                return _addDocumentationLinks;
-            }
-            set
-            {
-                _addDocumentationLinks = value;
-            }
-        }
+        public bool UseSigning { get; set; }
 
-        public bool UseSigning
-        {
-            get
-            {
-                return _useSigning;
-            }
-            set
-            {
-                _useSigning = value;
-            }
-        }
+        public bool AddTestApp { get; set; }
 
-        public bool AddTestApp
-        {
-            get
-            {
-                return _addTestApp;
-            }
-            set
-            {
-                _addTestApp = value;
-            }
-        }
+        public bool CreateXmlDocumentation { get; set; }
 
-        public bool CreateXmlDocumentation
-        {
-            get
-            {
-                return _createDocu;
-            }
-            set
-            {
-                _createDocu = value;
-            }
-        }
+        public bool ConvertOptionalsToObject { get; set; }
 
-        public bool ConvertOptionalsToObject
-        {
-            get
-            {
-                return _convertOptionalsToObject;
-            }
-            set
-            {
-                _convertOptionalsToObject = value;
-            }
-        }
+        public bool ConvertParamNamesToCamelCase { get; set; }
 
-        public bool ConvertParamNamesToCamelCase
-        {
-            get
-            {
-                return _convertParamNamesToCamelCase;
-            }
-            set
-            {
-                _convertParamNamesToCamelCase = value;
-            }
-        }
+        public bool RemoveRefAttribute { get; set; }
 
-        public bool RemoveRefAttribute
-        {
-            get
-            {
-                return _removeRefAttribute;
-            }
-            set
-            {
-                _removeRefAttribute = value;
-            }
-        }
+        public string Framework { get; set; }
 
-        public string Framework
-        {
-            get
-            {
-                return _framework;
-            }
-            set
-            {
-                _framework = value;
-            }
-        }
+        public string Folder { get; set; }
 
-        public string Folder
-        {
-            get 
-            {
-                return _folder;
-            }
-            set
-            {
-                _folder = value;
-            }
-        }
-
-        public bool OpenFolder
-        {
-            get
-            {
-                return _openFolder;
-            }
-            set
-            {
-                _openFolder = value;
-            }
-        }
+        public bool OpenFolder { get; set; }
 
         public int SupportByVersionSpacing { get; set; }
-
-        #endregion
     }
 }
--- CodeGenerator.Documentation/Settings.cs
+++ CodeGenerator.Documentation/Settings.cs
@@ -1,32 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace LateBindingApi.CodeGenerator.Documentation
 {
     class Settings
     {
-        #region Fields
-
-        string _folder;
-        
-        #endregion
-       
-        #region Properties
-
-        public string Folder
-        {
-            get
-            {
-                return _folder;
-            }
-            internal set 
-            {
-                _folder = value;
-            }
-        }
-
-        #endregion
+        public string Folder { get; internal set; }
     }
 }
