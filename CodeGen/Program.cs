9773a3f2a0f60b61b11c6884e98ca7e5e251a426
Jozef Izso <jozef.izso@gmail.com>
2020-06-05 15:04:40 +0200

    Stop codegen app only when debugger is attached

--- CodeGen/Program.cs
+++ CodeGen/Program.cs
@@ -102,7 +102,7 @@ namespace LateBindingApi.CodeGenerator.CodeGen
 
             Log.Info("Done.");
 
-            //if (Debugger.IsAttached)
+            if (Debugger.IsAttached)
             {
                 Console.ReadKey();
             }
